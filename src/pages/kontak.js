b1d653bee246a3d467f760b03c4e699d45c34098
zanovaexe22 <yayankyayank158@gmail.com>
2025-07-30 12:37:17 +0800

    Create kontak.js

---
+++ src/pages/kontak.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Kontak() {
+  return (
+    <div>
+      <h2>Kontak Kami</h2>
+      <p>Untuk menghubungi admin hubungi nomor pada link yang tertera di bawah:</p>
+      <a href="https://wa.me/qr/F4EXK3JITKODF1" target="_blank" rel="noopener noreferrer">
+        Hubungi via WhatsApp
+      </a>
+    </div>
+  );
+}
