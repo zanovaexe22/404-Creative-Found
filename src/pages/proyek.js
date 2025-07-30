ca14411031bb3e74687f530a3a699e87db8aaacc
zanovaexe22 <yayankyayank158@gmail.com>
2025-07-30 12:36:47 +0800

    Create proyek.js

---
+++ src/pages/proyek.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Proyek() {
+  return (
+    <div>
+      <h2>Proyek</h2>
+      <p>Sayang sekali belum ada proyek dan admin sedang pusing</p>
+    </div>
+  );
+}
